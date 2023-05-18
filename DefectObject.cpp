3890551e69ce271529d38d42de4de1abf2305d55
Masya <95611136+FeN1Kss@users.noreply.github.com>
2023-05-18 21:53:43 +0300

    Create DefectObject.cpp

---
+++ DefectObject.cpp
@@ -0,0 +1,72 @@
+#pragma once 
+#include "Defect.h"
+Defect::Defect()
+{}
+Defect::Defect(const std::string _name, const std::string _description, const int _priority, const bool _isCompleted, const int _severity) : severity(_severity)
+{}
+Defect::~Defect()
+{}
+void Defect::setSeverity(const int _severity)
+{
+	severity = _severity;
+}
+int Defect::getSeverity(void) const
+{
+	return severity;
+}
+
+std::istream& operator>>(std::istream& in, Defect& obj)
+{
+	std::string Name, Description;
+	int priority, severity;
+	bool isCompleted;
+
+	std::cout << "Name: ";
+	std::cin >> Name;
+	obj.setName(Name);
+
+	std::cout << "Description: ";
+	std::cin >> Description;
+	obj.setDescription(Description);
+
+	std::cout << "Severity: ";
+	std::cin >> severity;
+	obj.setSeverity(severity);
+
+	std::cout << "Priority: ";
+	std::cin >> priority;
+	while (std::cin.fail()) {
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Incorrect, try again: ";
+		std::cin >> priority;
+	}
+	obj.setPriority(priority);
+
+	std::cout << "IsCompleted(True - 1 or False - 0) : ";
+	std::cin >> isCompleted;
+	while (std::cin.fail()) {
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Incorrect, try again: ";
+		std::cin >> isCompleted;
+	}
+	obj.setIsCompleted(isCompleted);
+
+	return in;
+}
+void Defect::show(std::ostream& obj) const {
+	std::cout << "------------------------------------" << std::endl;
+	std::cout << "Name: " << name << std::endl;
+	std::cout << "Description: " << description << std::endl;
+	std::cout << "Priority: " << priority << std::endl;
+	std::cout << "IsCompleted: " << isCompleted << std::endl;
+	std::cout << "Severity: " << severity << std::endl;
+	std::cout << "------------------------------------" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& out, Defect& obj)
+{
+	obj.show(out);
+	return out;
+}
