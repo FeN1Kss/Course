ab787cc6db47e2a6cd62b75e33ea4e48a5669908
Masya <95611136+FeN1Kss@users.noreply.github.com>
2023-05-18 21:57:07 +0300

    Create PriorityQueueContainer.h

---
+++ PriorityQueueContainer.h
@@ -0,0 +1,27 @@
+#pragma once 
+#include "Defect.h"
+#include "DevelopmentTask.h"
+
+
+class PriorityQueue {
+private:
+    struct Node {
+        Task* task;
+        Node* next;
+    };
+
+    Node* head;
+
+public:
+    PriorityQueue();
+    ~PriorityQueue();
+
+    void push(Task* task);
+    void pop();
+    void print() const;
+    void inFile(const std::string& filename) const;
+    void fromFile(const std::string& filename);
+    void sort();
+    void Clear();
+    int countDefectsFromComponent(const int component) const;
+};
